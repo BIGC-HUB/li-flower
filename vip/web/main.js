4725444ea281f542d0c5843a0daa2e44a80f6d35
mer <iwangyang@qq.com>
2017-11-24 11:33:46 +0800

    170

--- vip/web/main.js
+++ vip/web/main.js
@@ -1,7 +1,15 @@
 Sea.Ajax({
-    url: '/user_sign_up',
+    url: '/user_login',
     data: {
-        "wx": {
+        "sms": true,
+        "sex": "女士",
+        "province": "北京市",
+        "city": "北京市",
+        "town": "宣武区",
+        "phone": "17396200129",
+        "phone_sms": "9357",
+        "sea_id": 1511426464515
+        wx: {
             "openid": "oeAUq1SEowuGLUciXOP3cu3pBPyY",
             "nickname": "大海",
             "sex": "先生",
@@ -12,7 +20,7 @@ Sea.Ajax({
             "headimgurl": "http:\/\/wx.qlogo.cn\/mmopen\/vi_32\/PiajxSqBRaEIN7ia30FDb9qCLNVZPOIfTuNGkBERlbwVndYERgBiaicOs7iaseobuaNLPNFos41pSNQY6KibFQgJgtZg\/0",
             "privilege": [],
             "unionid": "o-S7Z0hD3RZ7Xphthy3GiUSSLHdI",
-        },
+        }
     }
 }).then(res => {
     log(res)
